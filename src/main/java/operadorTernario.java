0c9c325003a091f109ebfa70fcbf2dc7cde609e2
mrdan99 <danielcorreagomes@gmail.com>
2023-09-21 14:56:35 -0300

    operadores ternários

---
+++ src/main/java/operadorTernario.java
@@ -0,0 +1,17 @@
+public class operadorTernario {
+    public static void main(String[] args) {
+
+        // classe Operadores.java
+        int a, b;
+
+        a = 6;
+        b = 6;
+
+        String resultado = a==b ?"verdadeiro" : "falso";
+
+
+        System.out.println(resultado);
+
+
+    }
+}
